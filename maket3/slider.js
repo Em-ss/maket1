0f14b7ba3d1114031e7ef3dda152e63119dfb6d3
maket1 <email1>
2022-07-13 00:47:31 +0300

    35commit

--- maket3/slider.js
+++ maket3/slider.js
@@ -1,6 +1,34 @@
-/*var swiper = new Swiper(".mySwiper", {
+ /* var swiper = new Swiper(".mySwiper", {
     pagination: {
       el: ".swiper-pagination",
       dynamicBullets: true,
     },
-  }); */
+  }); 
+  
+  let sliderAbout = document.querySelector(".mySwiper");
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    let newSwiper = new Swiper('.mySwiper', {
+      pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    });
+  } else {
+    sliderAbout.destroy();
+  }
+});
+var windowWidth = $(window).width();
+document.addEventListener('ready', () => { 
+  
+                if(window.innerWidth < 768) {
+    let newSwiper = new Swiper('.mySwiper', {
+      pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    });
+  } else {
+    sliderAbout.destroy();
+  }
+})*/
