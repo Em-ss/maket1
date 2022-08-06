ce4d537f8d2a0b3d71b66111fc47b988f2673757
maket1 <email1>
2022-08-06 23:07:30 +0300

    57commit

--- block7/src/js/index.js
+++
@@ -1,9 +0,0 @@
-console.log("Как интересно!")
-// import '../scss/style.scss';
-// console.log('Works!')
-// // E:\maket\index.html
-// var slider = document.querySelector('.slider')
-// import {click3,click,openBtn, openBtn2, openBtn3, allBtns, } from 'block7/menu.js';
-// import {list1, list2, list3, list4, list5, label, logo, list,} from 'block7/slider.js';
-// import 'block7/slider2.js';
-// import '.block7/slidder3.js';
