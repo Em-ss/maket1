ac98ae91f0a5ad35e4784f27cdcfd5be8ea7fb11
maket1 <email1>
2022-08-08 15:31:49 +0300

    new11

---
+++ webpack-static-template/src/js/index.js
@@ -0,0 +1,11 @@
+import '../scss/style.scss';
+
+console.log('Works!');
+
+
+import '../js/maket3.js';
+
+import '../js/slider.js';
+import '../js/slider2.js';
+import '../js/slider3.js';
+import '../js/menu.js';
